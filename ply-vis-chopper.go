c84ee8163a696ac051aa70f465fb977ec6273fd4
Exolon Voncor <git@exoz.net>
2021-01-02 21:02:04 +0100

    changes in main

--- ply-vis-chopper.go
+++ ply-vis-chopper.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"EXoloN/cloudcompare"
 	"EXoloN/plyreader"
 	"EXoloN/visio"
 	"fmt"
@@ -14,19 +13,20 @@ func open_files(args []string) (*os.File, *os.File, *os.File, *os.File) {
 
 	for c := 0; c < 3; c++ {
 		if out[c], err = os.Open(args[c]); err != nil {
-			panic(fmt.Sprint("Can't open input file:", err))
+			panic(fmt.Sprint("Can't open input file: ", err))
 		}
 	}
 
 	if out[3], err = os.OpenFile(args[3], os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0664); err != nil {
-		panic(fmt.Sprint("Can't open ouput file:", err))
+		panic(fmt.Sprint("Can't open ouput file: ", err))
 	}
 
 	return out[0], out[1], out[2], out[3]
 }
 
 func main() {
-	var original, cropped cloudcompare.PointCloud
+	// var original, cropped cloudcompare.PointCloud
+	var err error
 
 	if len(os.Args) != 5 {
 		panic("Must have four Args!")
@@ -34,9 +34,18 @@ func main() {
 
 	original_ply, cropped_ply, ply_vis, chopped_vis := open_files(os.Args[1:])
 
-	original = plyreader.ReadPLY(original_ply)
-	cropped = plyreader.ReadPLY(cropped_ply)
-	vis := visio.ReadVis(ply_vis)
+	original, err := plyreader.ReadPLY(original_ply)
+	if err != nil {
+		panic(err)
+	}
+	cropped, err := plyreader.ReadPLY(cropped_ply)
+	if err != nil {
+		panic(err)
+	}
+	vis, err := visio.ReadVis(ply_vis)
+	if err != nil {
+		panic(err)
+	}
 
 	positions := make([]int, cropped.Elements())
 
